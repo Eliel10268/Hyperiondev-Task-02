6e8e821945f277d0ef33d84d018ffdb886790ae3
Eliel10268 <ndaeliel123@gmail.com>
2023-03-21 23:01:17 +0000

    Hyperiondev Bootcamp Assignment 02

---
+++ replace.py
@@ -0,0 +1,16 @@
+
+#This will hold a phrase with exclamation marks
+phrase= "The quick!brown!fox!jumps!over!the!lazy!dog!."
+
+#this will replace exclamation marks by blank spaces
+clear_phrase = phrase.replace("!"," ")
+
+#this will display on the screen clear text without exclamation marks
+print(clear_phrase)
+#this will turn the letters into uppercase
+Upper_phrase = clear_phrase.upper()
+
+#this will display on the screen a text in capital letters/ uppercase
+print(Upper_phrase)
+#ths will display on the screen the text in reverse
+print(Upper_phrase[::-1])
---
+++ strings.py
@@ -0,0 +1,9 @@
+
+#this variable contain the value "$$$Superman$$$"
+hero =  "$$$Superman$$$"
+
+#this will delete the dollar sign in the "$$$Superman$$$"
+hero = hero.strip("$")
+
+#this will print the word "Superman" oon the screen
+print(hero)
